ed1a25633b43195aad2008976e30ac15dd3ee76b
lkkrd-laptop <leshkord@gmail.com>
2022-11-14 17:03:04 +0100

    Ramki zmieniają kolorki

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const colorCards = document.querySelectorAll(".color-card");
 const title = document.querySelector(".title");
+const container = document.querySelector(".container");
+const agreeBtns = document.querySelectorAll(".agree-btn");
 
 //funkcja dodaje eventListener do colorCard
 colorCards.forEach((card) => {
@@ -7,7 +9,13 @@ colorCards.forEach((card) => {
     "click",
     //funkcja zmienia kolor
     () => {
-      title.style.color = card.id;
+      let color = card.id;
+      title.style.color = color;
+      container.style.borderColor = color;
+      agreeBtns.forEach((btn) => {
+        btn.style.borderColor = color;
+      });
+
       switch (card.id) {
         case "green":
           console.log("Case: succes alert");
